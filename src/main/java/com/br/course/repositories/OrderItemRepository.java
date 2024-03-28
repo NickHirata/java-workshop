37ba62802c12bfd9513cd19014039513392e8ea8
NickHirata <nicolehirata123@gmail.com>
2024-03-28 08:12:23 -0300

    Ajustes

--- src/main/java/com/br/course/repositories/OrderItemRepository.java
+++ src/main/java/com/br/course/repositories/OrderItemRepository.java
@@ -3,9 +3,10 @@ package com.br.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.br.course.entities.OrderItem;
+import com.br.course.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
     
     
 } 
